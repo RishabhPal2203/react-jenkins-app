a4399e7962fa505fd9412696b75c0507e5130b47
RishabhPal2203 <rspal293@gmail.com>
2026-05-28 14:04:36 +0530

    added a groovy file for jenkins deployment

---
+++ jenkins.groovy
@@ -0,0 +1,19 @@
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Install Dependencies') {
+            steps {
+                bat 'npm install'
+            }
+        }
+
+        stage('Build Application') {
+            steps {
+                bat 'npm run build'
+            }
+        }
+
+    }
+}
